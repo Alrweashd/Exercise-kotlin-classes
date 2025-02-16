6f123689f72ceca22d83d93430a1e42b1cf2eb7b
alrweashd <trkyabler@gmail.com>
2025-02-16 18:30:54 +0300

    done with the challenge

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,21 @@
 fun main() {
     println("Hello World!")
+    val myBook = Book("World War 2","Turki",2025, "History");
+
+    println("myBook $myBook")
+    println(myBook.getBookInfo())
+
+}
+class Book(var title: String,
+           var author: String,
+           var yearPublished: Int,
+           var genre: String) {
+
+    override fun toString(): String {
+        return "Title: $title, Author:$author, Year: $yearPublished"
+    }
+    fun getBookInfo():String {
+        return "$title by $author ($yearPublished) genre: $genre"
+    }
+
 }
